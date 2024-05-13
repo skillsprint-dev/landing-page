a876f20a6779a6cf8e3f1c0237430499c44e40b9
dohsimpson <mydohsimpson@gmail.com>
2024-05-12 22:07:27 -0400

    bug fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,9 +27,6 @@ export default function Hero() {
               </div>
           </div>
         </div>
-        {/* <svg className="animate-bounce w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"> */}
-        {/*   <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path> */}
-        {/* </svg> */}
       </section>
     </>
   )
